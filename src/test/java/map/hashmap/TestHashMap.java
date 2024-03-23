a70803f2dd03502734e8bee39fb11498e4bfb96a
Krxk <1246924370@qq.com>
2024-03-23 16:28:52 +0800

    修改 ConcorrentHashMap

--- src/test/java/map/hashmap/TestHashMap.java
+++ src/test/java/map/hashmap/TestHashMap.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 
 import java.util.HashMap;
 import java.util.LinkedHashMap;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class TestHashMap {
     @Test
@@ -11,6 +12,7 @@ public class TestHashMap {
         HashMap<String, Integer> hashMap = new HashMap<>();
         hashMap.get(2);
         LinkedHashMap<String, Integer> linkedHashMap = new LinkedHashMap<>();
+//        ConcurrentHashMap
 //        linkedHashMap.put();
 //        linkedHashMap.get()
     }
